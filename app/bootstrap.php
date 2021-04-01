9a9847dd99597320f70f3085ea42d431ee9f5156
cdurau <{ID}+{username}@users.noreply.github.com>
2021-04-01 21:28:02 +0200

    + Updated PHP Version + Docker Port + Core Class

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 
 // Load Libraries
-require_once 'libraries/Core.php';
-require_once 'libraries/Controller.php';
-require_once 'libraries/Database.php';
+require_once 'libraries/core.php';
+require_once 'libraries/controller.php';
+require_once 'libraries/database.php';
