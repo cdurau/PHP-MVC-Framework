38f1ac4ae635a35175201a4a37e39d12fdc005c0
cdurau <{ID}+{username}@users.noreply.github.com>
2021-04-02 18:05:09 +0200

    + Pages Controller

--- app/libraries/core.php
+++ app/libraries/core.php
@@ -6,17 +6,38 @@
    */
 class Core
 {
-  protected $currentController = 'Pages';
-  protected $currentMethod = 'index';
-  protected $params = [];
+    protected $currentController = 'Pages';
+    protected $currentMethod = 'index';
+    protected $params = [];
 
-  public function __construct()
-  {
-    $this->getUrl();
-  }
+    public function __construct()
+    {
+        // If url is set
+        if (isset($_GET['url'])) {
+            $url = $this->getUrl();
 
-  public function getUrl()
-  {
-    echo $_GET['url'];
-  }
+            // Look in controllers for first value of $url
+            if (file_exists('../app/controllers/' . ucwords($url[0]) . '.php')) {
+                // If exists, set as current Controller
+                $this->currentController = ucwords($url[0]);
+
+                // Unset Index 0
+                unset($url[0]);
+            }
+        }
+
+        // Require the controller
+        require_once '../app/controllers/' . $this->currentController . '.php';
+
+        // Instantiate controller class
+        $this->currentController = new $this->currentController;
+    }
+
+    public function getUrl()
+    {
+        $url = rtrim($_GET['url'], '/');
+        $url = filter_var($url, FILTER_SANITIZE_URL);
+        $url = explode('/', $url);
+        return $url;
+    }
 }
